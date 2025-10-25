8b481584d4c58a8ebeb5477ce1d4d82a3922decb
eduardogomesDev21 <eduardogmsnishida1@gmail.com>
2025-10-25 17:17:25 -0300

    Coloca os botoes na tela de chatbot

--- Prot_TG_FrontEnd/src/app/chatbot/chatbot.component.ts
+++ Prot_TG_FrontEnd/src/app/chatbot/chatbot.component.ts
@@ -200,4 +200,11 @@ export class ChatbotComponent implements OnInit, OnDestroy {
     if (this.isBrowser) localStorage.removeItem('nutrinow_session_id');
     this.authService.logout().subscribe(() => this.router.navigate(['/login']));
   }
+  goToProfile() {
+    window.location.href = 'http://localhost:4200/perfil';
+  }
+
+  goToDietaTreino() {
+    window.location.href = 'http://localhost:4200/dieta-treino';
+  }
 }
