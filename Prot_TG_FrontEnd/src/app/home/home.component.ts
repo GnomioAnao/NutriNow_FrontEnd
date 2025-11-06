638db553822f7b7be59beca634367bc0e0e6123c
Caio Lima <caiolimacruz@gmail.com>
2025-11-06 20:37:49 -0300

    atualizações do visual da home

--- Prot_TG_FrontEnd/src/app/home/home.component.ts
+++ Prot_TG_FrontEnd/src/app/home/home.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [RouterModule],
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent {
+  
+// }
 
 @Component({
   selector: 'app-home',
@@ -9,5 +20,25 @@ import { RouterModule, Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleFaq(event: Event): void {
+    const clickedItem = (event.currentTarget as HTMLElement).closest('.faq-item');
+    if (!clickedItem) return;
+
+    const allFaqs = document.querySelectorAll('.faq-item');
+
+    allFaqs.forEach(faq => {
+      if (faq !== clickedItem) {
+        this.renderer.removeClass(faq, 'active');
+      }
+    });
+
+    if (clickedItem.classList.contains('active')) {
+      this.renderer.removeClass(clickedItem, 'active');
+    } else {
+      this.renderer.addClass(clickedItem, 'active');
+    }
+  }
 }
